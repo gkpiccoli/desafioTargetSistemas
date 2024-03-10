c7b8e84b2cc6ce467cd58d3871b93157f196ef08
gkpiccoli <gustavokpiccoli@gmail.com>
2024-03-10 20:11:43 -0300

    add more comments

--- app/src/main/java/com/gkpiccoli/desafiotargetsistemas/QuestaoCinco.java
+++ app/src/main/java/com/gkpiccoli/desafiotargetsistemas/QuestaoCinco.java
@@ -3,30 +3,32 @@ import java.util.Scanner;
 
 public class QuestaoCinco {
     public static void main(String[] args) {
+        //Cria um objeto Scanner para ler o input do usuário
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite uma string: ");
         String originalString = scanner.nextLine();
         scanner.close();
-
+        //Chama o método reversedString para inverter a string fornecida
         String reversedString = reverseString(originalString);
         System.out.println("String original: " + originalString);
         System.out.println("String invertida: " + reversedString);
     }
 
     public static String reverseString(String str) {
+        //Converte a string em array de caracteres
         char[] charArray = str.toCharArray();
         int left = 0;
         int right = charArray.length - 1;
 
         while (left < right) {
-            // Troca os caracteres da frase da esquerda para direita
+            // Troca os caracteres  da esquerda para a direita
             char temp = charArray[left];
             charArray[left] = charArray[right];
             charArray[right] = temp;
             left++;
             right--;
         }
-
+        //Converte o array de volta para um string e a retorna
         return new String(charArray);
     }
 }
